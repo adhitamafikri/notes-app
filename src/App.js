3fa26f50d7221cedbfe8cbdc754aa0846cc3d3aa
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2023-01-07 00:27:06 +0700

    can show notes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import NoteCard from "./components/NoteCard";
 import NoteForm from "./components/NoteForm";
 import "./App.css";
 
+// note: {
+//   title: "",
+//   body: "",
+//   color: "white",
+// }
+
 function App() {
+  /**
+   * @returns {Array}
+   */
   const notes = useMemo(() => {
     const result = localStorage.getItem("notes");
     if (!!result) {
@@ -15,8 +24,20 @@ function App() {
   return (
     <div className="App">
       <NoteForm />
-      <NoteCard title="Example" body="This is body of note" color="beige" />
-      <NoteCard title={24} body="Body ke dua" color="beige" />
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+          marginTop: 56,
+        }}
+      >
+        {!!notes.length > 0 &&
+          notes.map((note) => (
+            <NoteCard title={note.title} body={note.body} color={note.color} />
+          ))}
+      </div>
     </div>
   );
 }
